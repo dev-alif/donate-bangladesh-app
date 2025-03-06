0174dd3c63a03c2b60de825638ccc37b9c38ec6e
dev-alif <appdev.alif@gmail.com>
2025-03-07 04:51:31 +0600

    donation feature added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-// document.getElementById()
+document.getElementById('btn-donation-1')
+    .addEventListener('click', function(){
+        const inputFieldValue = parseFloat(getInputFieldValueById('donation-amount-1'));
+        const textFieldValue = parseFloat(getTextFieldValueById('total-donation-amount-1'));
+        const mainBalance = parseFloat(getTextFieldValueById('main-balance'));
+        
+        const newDonationBalance = textFieldValue + inputFieldValue;
+        const newMainBalance = mainBalance - inputFieldValue;
+
+        document.getElementById('total-donation-amount-1').innerText = newDonationBalance;
+        document.getElementById('main-balance').innerText = newMainBalance;
+    });
+
+document.getElementById('btn-donation-2')
+    .addEventListener('click', function(){
+        const inputFieldValue = parseFloat(getInputFieldValueById('donation-amount-2'));
+        const textFieldValue = parseFloat(getTextFieldValueById('total-donation-amount-2'));
+        const mainBalance = parseFloat(getTextFieldValueById('main-balance'));
+        
+        const newDonationBalance = textFieldValue + inputFieldValue;
+        const newMainBalance = mainBalance - inputFieldValue;
+
+        document.getElementById('total-donation-amount-2').innerText = newDonationBalance;
+        document.getElementById('main-balance').innerText = newMainBalance;
+    });
+
+document.getElementById('btn-donation-3')
+    .addEventListener('click', function(){
+        const inputFieldValue = parseFloat(getInputFieldValueById('donation-amount-3'));
+        const textFieldValue = parseFloat(getTextFieldValueById('total-donation-amount-3'));
+        const mainBalance = parseFloat(getTextFieldValueById('main-balance'));
+        
+        const newDonationBalance = textFieldValue + inputFieldValue;
+        const newMainBalance = mainBalance - inputFieldValue;
+
+        document.getElementById('total-donation-amount-3').innerText = newDonationBalance;
+        document.getElementById('main-balance').innerText = newMainBalance;
+    });
---
+++ js/utilities.js
@@ -0,0 +1,10 @@
+function getInputFieldValueById(id){
+    const inputFieldValue = document.getElementById(id).value;
+    return inputFieldValue;
+}
+
+function getTextFieldValueById(id){
+    const textFieldValue = document.getElementById(id).innerText;
+    return textFieldValue;
+}
+
